d7fe0b94208ba8b6fd92f9d3b2889757894669c0
minkmg0605 <minkmg0605@naver.com>
2023-09-28 15:44:44 +0900

    Weatherapp

---
+++ utils/WeatherConditions.js
@@ -0,0 +1,114 @@
+export const weatherConditions= {
+	0:{
+		title : 'Clear sky',
+		icon : "http://openweathermap.org/img/wn/01d@2x.png",
+	},
+	1:{
+		title :"Mainly Sunny",
+		icon :"http://openweathermap.org/img/wn/01d@2x.png",
+	},
+	2:{
+		title :"Partly Cloudy",
+		icon :"http://openweathermap.org/img/wn/02d@2x.png",
+	},
+	3:{
+		title :"Cloudy",
+		icon :"http://openweathermap.org/img/wn/03d@2x.png",		
+	},
+	45:{
+		title :"Foggy",
+		icon :"http://openweathermap.org/img/wn/50d@2x.png",
+	},
+	48:{
+        title :"Rime Fog",
+        icon :"http://openweathermap.org/img/wn/50d@2x.png",
+	},
+	51:{
+        title :"Light Drizzle",
+		icon :"http://openweathermap.org/img/wn/09d@2x.png",
+	},
+	53:{
+		title :"Drizzle",
+		icon :"http://openweathermap.org/img/wn/09d@2x.png",
+	},
+	55:{
+        title :"Heavy Drizzle",
+        icon :"http://openweathermap.org/img/wn/09d@2x.png",
+	},
+	56:{
+        title :"Light Freezing Drizzle",
+        icon :"http://openweathermap.org/img/wn/09d@2x.png",
+	},
+	57:{
+        title :"Freezing Drizzle",
+		icon :"http://openweathermap.org/img/wn/09d@2x.png",
+	},
+	61:{
+        title :"Light Rain",
+        icon :"http://openweathermap.org/img/wn/10d@2x.png",
+	},
+	63:{
+		title :"Rain",
+		icon :"http://openweathermap.org/img/wn/10d@2x.png"
+	},
+	65:{
+        title :"Heavy Rain",
+        icon :"http://openweathermap.org/img/wn/10d@2x.png"
+	},
+	66:{
+        title :"Freezing Rain",
+        icon :"http://openweathermap.org/img/wn/10d@2x.png"
+	},
+	67:{
+        title :"Freezing Rain",
+        icon :"http://openweathermap.org/img/wn/10d@2x.png"
+	},
+	71:{
+        title :"Light Snow",
+        icon :"http://openweathermap.org/img/wn/13d@2x.png"
+	},
+	73:{
+        title :"Snow",
+        icon :"http://openweathermap.org/img/wn/13d@2x.png"
+	},
+	75:{
+        title :"Heavy Snow",
+        icon :"http://openweathermap.org/img/wn/13d@2x.png"
+	},
+	77:{
+		title :"Snow Grains",
+		icon:"http://openweathermap.org/img/wn/13d@2x.png"
+	},
+	80:{
+		title :"Light Showers",
+		icon:"http://openweathermap.org/img/wn/09d@2x.png"
+	},
+	81:{
+		title :"Showers",
+		icon:"http://openweathermap.org/img/wn/09d@2x.png"
+	},
+	82:{
+		title :"Heavy Showers",
+		icon:"http://openweathermap.org/img/wn/09d@2x.png"
+	},
+	85:{
+		title :"Snow Showers",
+		icon:"http://openweathermap.org/img/wn/13d@2x.png"
+	},
+	86:{
+		title :"Snow Showers",
+		icon:"http://openweathermap.org/img/wn/13d@2x.png"
+	},
+	95:{
+		title :"Thunderstorm",
+		icon:"http://openweathermap.org/img/wn/11d@2x.png"
+	},
+	96:{
+		title :"Thunderstorm With Hail",
+		icon:"http://openweathermap.org/img/wn/11d@2x.png"
+	},
+	99:{
+		title :"Thunderstorm With Hail",
+		icon:"http://openweathermap.org/img/wn/11d@2x.png"
+	}
+}
